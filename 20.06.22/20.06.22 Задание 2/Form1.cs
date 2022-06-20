efb4aa704ffc688d0dcfd7ee7ec47c02b600bc4f
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-06-20 15:46:38 +0300

    Add files via upload

---
+++ 20.06.22/20.06.22 Задание 2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20._06._22_Задание_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long x = Convert.ToInt32(textBox1.Text);
+            textBox2.Text = Convert.ToString(Factorial(x));
+        }
+        long Factorial(long n)
+        {
+            if (n == 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
