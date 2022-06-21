01acb546ea9fb2c88a32565b4f0e30a6c8f9b7f2
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-06-21 11:26:04 +0300

    Add files via upload

---
+++ 15.06.22/15.06.22 Задание 5/Invoice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._06._22_Задание_5
+{
+    class Invoice
+    {
+        private readonly int Account;
+        private readonly string Customer;
+        private readonly string Provider;
+        private string Article;
+        private int Quantity;
+        public Invoice(int account, string customer, string provider, string article, int quantity)
+        {
+            Account = account;
+            Customer = customer;
+            Provider = provider;
+            Article = article;
+            Quantity = quantity;
+        }
+        public double GetAccountWithNds(double nds = 0.3)
+        {
+            return Account + (Account * nds);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{Customer} заказал у {Provider} {Quantity} {Article} и выставил счет,");
+            Console.WriteLine($"который будет составлять {Account} без Ндс, и {GetAccountWithNds()} с Ндс");
+        }
+    }
+}
+
