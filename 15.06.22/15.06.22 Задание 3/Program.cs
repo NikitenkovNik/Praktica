e1f80447c921bee39bbc2056178d3dd65aa84e96
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-06-21 11:40:35 +0300

    Add files via upload

---
+++ 15.06.22/15.06.22 Задание 3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._06._22_Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd;
+            double eur;
+            double rub;
+            Console.Write("Курс доллара к гривне: ");
+            usd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Курс евро к гривне: ");
+            eur = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Курс рубля к гривне: ");
+            rub = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Converter converter = new Converter(usd, eur, rub);
+            Console.WriteLine("Какое действие вы хотите произвести:");
+            Console.WriteLine("1 - гривны в доллары");
+            Console.WriteLine("2 - гривны в евро");
+            Console.WriteLine("3 - гривны в рубли");
+            Console.WriteLine("4 - доллары в гривны");
+            Console.WriteLine("5 - евро в гривны");
+            Console.WriteLine("6 - рубли в гривны");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int sum;
+            switch (x)
+            {
+                case 1:
+                    Console.Write("Сумма в гривнах: ");
+                    sum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("результат: " + converter.UAH_USD(sum));
+                    break;
+                case 2:
+                    Console.Write("Сумма в гривнах: ");
+                    sum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("результат: " + converter.UAH_EUR(sum));
+                    break;
+                case 3:
+                    Console.Write("Сумма в гривнах: ");
+                    sum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("результат: " + converter.UAH_RUB(sum));
+                    break;
+                case 4:
+                    Console.Write("Сумма в долларах: ");
+                    sum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("результат: " + converter.USD_UAH(sum));
+                    break;
+                case 5:
+                    Console.Write("Сумма в евро: ");
+                    sum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("результат: " + converter.EUR_UAH(sum));
+                    break;
+                case 6:
+                    Console.Write("Сумма в рублях: ");
+                    sum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("результат: " + converter.RUB_UAH(sum));
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
+
---
+++ 15.06.22/15.06.22 Задание 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._06._22_Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Информация о клиенте: ");
+            string client = Console.ReadLine();
+            Console.Write("Информация о поставщике: ");
+            string postav = Console.ReadLine();
+            Console.Write("Выставленный счет: ");
+            int summa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Информация о товаре: ");
+            string tovar = Console.ReadLine();
+            Console.Write("Количество товара: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Invoice invoice = new Invoice(summa, client, postav, tovar, k);
+            invoice.Show();
+            Console.ReadKey();
+        }
+    }
+}
+    
