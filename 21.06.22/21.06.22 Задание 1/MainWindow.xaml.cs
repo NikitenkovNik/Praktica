956ff6971656a6e283edf827e0d4b6efa71b537f
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-06-21 11:48:13 +0300

    Add files via upload

---
+++ 21.06.22/21.06.22 Задание 1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace _21._06._22_Задание_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Directory.CreateDirectory("Folder_" + i);
+                }
+                L1.Content = "Папки появились";
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void B2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Directory.Delete("Folder_" + i);
+                }
+                L1.Content = "Папок больше нет";
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+    }
+}
---
+++ 21.06.22/21.06.22 Задание 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _21._06._22_Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("NowiyFayl.txt");
+            Console.WriteLine("Введите данные для записи: ");
+            sw.WriteLine(Console.ReadLine());
+            sw.Close();
+            Console.Clear();
+            StreamReader sr = new StreamReader("NowiyFayl.txt");
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
