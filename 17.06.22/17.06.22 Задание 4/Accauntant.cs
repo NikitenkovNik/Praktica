a2971779240995ed441aac942beed8ac187f2c9a
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-06-29 09:19:34 +0300

    Add files via upload

---
+++ 17.06.22/17.06.22 Задание 4/Accauntant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._06._22_Задание_4
+{
+    class Accauntant
+    {
+        public static bool AskForBonus(Employees postWorker, int hours)
+        {
+            if (hours >= (int)postWorker)
+                return true;
+            else
+                return false;
+        }
+    }
+    public enum Employees
+    {
+        ChiefExecutiveOfficer = 140,
+        ChiefTechnologyOfficer = 130,
+        JuniorDeveloper = 50,
+        MiddleDeveloper = 70,
+        SeniorDeveloper = 90,
+        QualityAssuranceEngineers = 110
+    }
+}
+
