d01a812519352fcbae05c46ac2a131c3dda94343
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-06-29 09:13:14 +0300

    Add files via upload

---
+++ 16.06.22/16.02.22 Задание 2/Article.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._02._22_Задание_2
+{
+    class Article
+    {
+        private string name;
+        private string shop;
+        private double price;
+        public Article(string Name, string Shop, double Price)
+        {
+            name = Name;
+            shop = Shop;
+            price = Price;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Shop
+        {
+            get
+            {
+                return shop;
+            }
+            set
+            {
+                shop = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
+
