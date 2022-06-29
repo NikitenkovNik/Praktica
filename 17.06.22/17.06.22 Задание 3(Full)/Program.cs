be3e181458bbccb9f35c09c2fd2ca44f8a599bfd
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-06-29 09:17:23 +0300

    Add files via upload

---
+++ 17.06.22/17.06.22 Задание 3(Full)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._06._22_Задание_3_Full_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Укажите цвет: (0 - синий, 1 - красный, 2 - зелёный, 3 - жёлтый)");
+            int color = Convert.ToInt32(Console.ReadLine());
+            ColorPrint.Print(stroka, color);
+            Console.ReadKey();
+        }
+    }
+}
