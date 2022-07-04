c3922f38e33092059b97172c5a366f150add94e6
NikitenkovNik <107553914+NikitenkovNik@users.noreply.github.com>
2022-07-04 09:18:48 +0300

    Add files via upload

---
+++ 28.06.22/28.06.22 Задание 2/Calc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._06._22_Задание_2
+{
+    class Calc
+    {
+        private double s1;
+        private double s2;
+        private double res;
+        public double S1
+        {
+            set
+            {
+                s1 = value;
+            }
+            get
+            {
+                return s1;
+            }
+        }
+        public double S2
+        {
+            set
+            {
+                s2 = value;
+            }
+            get
+            {
+                return s2;
+            }
+        }
+        public double Res
+        {
+            get
+            {
+                return res;
+            }
+        }
+        public void Sum()
+        {
+            res = s1 + s2;
+        }
+        public void Diff()
+        {
+            res = s1 - s2;
+        }
+        public void Mult()
+        {
+            res = s1 * s2;
+        }
+        public void Div()
+        {
+            res = s1 / s2;
+        }
+    }
+}
+
